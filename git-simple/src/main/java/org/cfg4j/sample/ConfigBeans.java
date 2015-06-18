34e17554d1e990a1ffd1f703a910a2d21167f447
Norbert Potocki <norbert.potocki@nort.pl>
2015-06-17 23:01:38 -0700

    use new PeriodicalRefreshStrategy constructor

--- git-simple/src/main/java/org/cfg4j/sample/ConfigBeans.java
+++ git-simple/src/main/java/org/cfg4j/sample/ConfigBeans.java
@@ -24,6 +24,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.concurrent.TimeUnit;
+
 @Configuration
 public class ConfigBeans {
 
@@ -38,7 +40,7 @@ public class ConfigBeans {
 
     return new ConfigurationProviderBuilder()
         .withConfigurationSource(source)
-        .withRefreshStrategy(new PeriodicalRefreshStrategy(5000))
+        .withRefreshStrategy(new PeriodicalRefreshStrategy(5, TimeUnit.SECONDS))
         .build();
   }
 
