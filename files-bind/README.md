aae3ee41fb8c152a8375f9ab6bc0d732eef6d213
Norbert Potocki <norbert.potocki@nort.pl>
2015-06-18 22:55:21 -0700

    files-bind example

---
+++ files-bind/README.md
@@ -0,0 +1,59 @@
+# [cfg4j](http://cfg4j.org) sample app (uses **local files** as configuration store)
+App demonstrating how to access configuration stored in a local files using cfg4j library. **Uses multiple configuration files.**
+
+## Usage
+```
+> cd cfg4j-sample-apps/
+> ./gradlew build
+> java -jar files-bind/build/libs/files-bind-1.0.0-SNAPSHOT.jar
+```
+
+You can:
+* use any path to config files: **-DconfigFilesPath=\<configFilesPath\>** parameter. Both absolute and relative paths are supported.
+
+```
+> java -DconfigFilesPath=./files-bind/build/libs/ -jar files-bind/build/libs/files-bind-1.0.0-SNAPSHOT.jar
+```
+
+
+## Code snippets
+
+### Configuration beans - see ConfigBeans.java
+```java
+@Value("${configFilesPath:./files-bind/build/libs/}")
+private String filesPath; // Run with -DconfigFilesPath=<configFilesPath> parameter to override
+
+@Bean
+public ConfigurationProvider configurationProvider() {
+  // Specify which files to load. Configuration from both files will be merged.
+  ConfigFilesProvider configFilesProvider = () -> Arrays.asList(new File("application.properties"), new File("otherConfig.properties"));
+
+  // Use local files as configuration store
+  ConfigurationSource source = new FilesConfigurationSource(configFilesProvider);
+
+  // Use relative paths
+  Environment environment = new ImmutableEnvironment(filesPath);
+
+  // Reload configuration every 5 seconds
+  RefreshStrategy refreshStrategy = new PeriodicalRefreshStrategy(5, TimeUnit.SECONDS);
+
+  // Create provider
+  return new ConfigurationProviderBuilder()
+      .withConfigurationSource(source)
+      .withRefreshStrategy(refreshStrategy)
+      .withEnvironment(environment)
+      .build();
+}
+```
+
+### Obtain configuration - see MainController.java
+```java
+// Define configuration interface 
+public interface ReksioConfig {
+  URL homepage();
+  ...
+}
+
+ReksioConfig reksioConfig = configurationProvider.bind("reksio", ReksioConfig.class);
+URL url = reksioConfig.homepage();
+```
---
+++ files-bind/src/main/java/org/cfg4j/sample/ConfigBeans.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2015 Norbert Potocki (norbert.potocki@nort.pl)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cfg4j.sample;
+
+import org.cfg4j.provider.ConfigurationProvider;
+import org.cfg4j.provider.ConfigurationProviderBuilder;
+import org.cfg4j.source.ConfigurationSource;
+import org.cfg4j.source.context.Environment;
+import org.cfg4j.source.context.ImmutableEnvironment;
+import org.cfg4j.source.files.FilesConfigurationSource;
+import org.cfg4j.source.git.ConfigFilesProvider;
+import org.cfg4j.source.refresh.RefreshStrategy;
+import org.cfg4j.source.refresh.strategy.PeriodicalRefreshStrategy;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class ConfigBeans {
+
+  @Value("${configFilesPath:./files-bind/build/libs/}")
+  private String filesPath; // Run with -DconfigFilesPath=<configFilesPath> parameter to override
+
+  @Bean
+  public ConfigurationProvider configurationProvider() {
+    // Specify which files to load. Configuration from both files will be merged.
+    ConfigFilesProvider configFilesProvider = () -> Arrays.asList(new File("application.properties"), new File("otherConfig.properties"));
+
+    // Use local files as configuration store
+    ConfigurationSource source = new FilesConfigurationSource(configFilesProvider);
+
+    // Use relative paths
+    Environment environment = new ImmutableEnvironment(filesPath);
+
+    // Reload configuration every 5 seconds
+    RefreshStrategy refreshStrategy = new PeriodicalRefreshStrategy(5, TimeUnit.SECONDS);
+
+    // Create provider
+    return new ConfigurationProviderBuilder()
+        .withConfigurationSource(source)
+        .withRefreshStrategy(refreshStrategy)
+        .withEnvironment(environment)
+        .build();
+  }
+}
