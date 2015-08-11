54836c0273e2bb05881a456d5b0876729d1cc91c
Norbert Potocki <norbert.potocki@nort.pl>
2015-08-11 04:08:35 -0700

    fix files-bind after upgrade

--- files-bind/src/main/java/org/cfg4j/sample/ConfigBeans.java
+++ files-bind/src/main/java/org/cfg4j/sample/ConfigBeans.java
@@ -18,17 +18,17 @@ package org.cfg4j.sample;
 import org.cfg4j.provider.ConfigurationProvider;
 import org.cfg4j.provider.ConfigurationProviderBuilder;
 import org.cfg4j.source.ConfigurationSource;
-import org.cfg4j.source.context.Environment;
-import org.cfg4j.source.context.ImmutableEnvironment;
+import org.cfg4j.source.context.environment.Environment;
+import org.cfg4j.source.context.environment.ImmutableEnvironment;
+import org.cfg4j.source.context.filesprovider.ConfigFilesProvider;
 import org.cfg4j.source.files.FilesConfigurationSource;
-import org.cfg4j.source.git.ConfigFilesProvider;
-import org.cfg4j.source.refresh.RefreshStrategy;
-import org.cfg4j.source.refresh.strategy.PeriodicalRefreshStrategy;
+import org.cfg4j.source.reload.ReloadStrategy;
+import org.cfg4j.source.reload.strategy.PeriodicalReloadStrategy;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.File;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
@@ -41,7 +41,7 @@ public class ConfigBeans {
   @Bean
   public ConfigurationProvider configurationProvider() {
     // Specify which files to load. Configuration from both files will be merged.
-    ConfigFilesProvider configFilesProvider = () -> Arrays.asList(new File("application.yaml"), new File("otherConfig.properties"));
+    ConfigFilesProvider configFilesProvider = () -> Arrays.asList(Paths.get("application.yaml"), Paths.get("otherConfig.properties"));
 
     // Use local files as configuration store
     ConfigurationSource source = new FilesConfigurationSource(configFilesProvider);
@@ -50,12 +50,12 @@ public class ConfigBeans {
     Environment environment = new ImmutableEnvironment(filesPath);
 
     // Reload configuration every 5 seconds
-    RefreshStrategy refreshStrategy = new PeriodicalRefreshStrategy(5, TimeUnit.SECONDS);
+    ReloadStrategy refreshStrategy = new PeriodicalReloadStrategy(5, TimeUnit.SECONDS);
 
     // Create provider
     return new ConfigurationProviderBuilder()
         .withConfigurationSource(source)
-        .withRefreshStrategy(refreshStrategy)
+        .withReloadStrategy(refreshStrategy)
         .withEnvironment(environment)
         .build();
   }
