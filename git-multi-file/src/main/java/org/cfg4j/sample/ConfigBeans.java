802610403c92bcbd68e324086c085f06afb1a44e
Norbert Potocki <norbert.potocki@nort.pl>
2015-08-11 04:16:00 -0700

    fix git-multi-file after upgrade

--- git-multi-file/src/main/java/org/cfg4j/sample/ConfigBeans.java
+++ git-multi-file/src/main/java/org/cfg4j/sample/ConfigBeans.java
@@ -18,17 +18,17 @@ package org.cfg4j.sample;
 import org.cfg4j.provider.ConfigurationProvider;
 import org.cfg4j.provider.ConfigurationProviderBuilder;
 import org.cfg4j.source.ConfigurationSource;
-import org.cfg4j.source.context.Environment;
-import org.cfg4j.source.context.ImmutableEnvironment;
-import org.cfg4j.source.git.ConfigFilesProvider;
+import org.cfg4j.source.context.environment.Environment;
+import org.cfg4j.source.context.environment.ImmutableEnvironment;
+import org.cfg4j.source.context.filesprovider.ConfigFilesProvider;
 import org.cfg4j.source.git.GitConfigurationSourceBuilder;
-import org.cfg4j.source.refresh.RefreshStrategy;
-import org.cfg4j.source.refresh.strategy.PeriodicalRefreshStrategy;
+import org.cfg4j.source.reload.ReloadStrategy;
+import org.cfg4j.source.reload.strategy.PeriodicalReloadStrategy;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.File;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
@@ -44,7 +44,7 @@ public class ConfigBeans {
   @Bean
   public ConfigurationProvider configurationProvider() {
     // Specify which files to load. Configuration from both files will be merged.
-    ConfigFilesProvider configFilesProvider = () -> Arrays.asList(new File("application.properties"), new File("otherConfig.properties"));
+    ConfigFilesProvider configFilesProvider = () -> Arrays.asList(Paths.get("application.properties"), Paths.get("otherConfig.properties"));
 
     // Use Git repository as configuration store
     ConfigurationSource source = new GitConfigurationSourceBuilder()
@@ -56,13 +56,13 @@ public class ConfigBeans {
     Environment environment = new ImmutableEnvironment(branch);
 
     // Reload configuration every 5 seconds
-    RefreshStrategy refreshStrategy = new PeriodicalRefreshStrategy(5, TimeUnit.SECONDS);
+    ReloadStrategy refreshStrategy = new PeriodicalReloadStrategy(5, TimeUnit.SECONDS);
 
     // Create provider
     return new ConfigurationProviderBuilder()
         .withConfigurationSource(source)
         .withEnvironment(environment)
-        .withRefreshStrategy(refreshStrategy)
+        .withReloadStrategy(refreshStrategy)
         .build();
   }
 }
